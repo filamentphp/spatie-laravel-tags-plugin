d2feab2e54c0ccfe5c5b51671c1b03b14616fab6
Dan <dan@danharrin.com>
2021-12-20 01:18:48 +0000

    Merge branch 2.x of https://github.com/laravel-filament/filament into 2.x

---
+++ src/Tables/Columns/SpatieTagsColumn.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Filament\Tables\Columns;
+
+class SpatieTagsColumn extends TagsColumn
+{
+    protected ?string $type = null;
+
+    public function getTags(): array
+    {
+        $type = $this->getType();
+        $tags = $this->getRecord()->tagsWithType($type);
+
+        return $tags->pluck('name')->toArray();
+    }
+
+    public function type(string | callable $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+}
